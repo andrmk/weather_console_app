f7a722e683201882c37dd63b21732794dce04aad
Andrei Romanchuk <andrei.rmk@gmail.com>
2023-08-27 16:54:57 +0300

    city

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+void main(List<String> arguments) {
+  if (arguments.length != 1) {
+    print('Syntax error: dart bin/main.dart <city>');
+    return;
+  }
 
-
-void main(List<String> arguments) {}
+  final cityName = arguments.first;
+  print(cityName);
+}
